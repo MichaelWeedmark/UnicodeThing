2cab9f018c591eb012de4f4e63e8d38e00a5e757
MichaelWeedmark <michael.weedmak@ocsbstudent.ca>
2015-10-16 14:32:57 -0400

    Unicode

---
+++ UnicodeThing/Unicode.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace UnicodeThing
+{
+    partial class frmUnicode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lstList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(95, 12);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lstList
+            // 
+            this.lstList.FormattingEnabled = true;
+            this.lstList.Location = new System.Drawing.Point(34, 48);
+            this.lstList.Name = "lstList";
+            this.lstList.Size = new System.Drawing.Size(210, 342);
+            this.lstList.TabIndex = 1;
+            // 
+            // frmUnicode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 423);
+            this.Controls.Add(this.lstList);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "frmUnicode";
+            this.Text = "Unicode";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.ListBox lstList;
+    }
+}
+
---
+++ UnicodeThing/Unicode.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Created by: Michael Weedmark
+ * Created on: 16-10-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit 3-11
+ * This program displays Unicode
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnicodeThing
+{
+    public partial class frmUnicode : Form
+    {
+        public frmUnicode()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            string letter;
+
+            for (int number = 65; number <= 90; number++)
+            {
+                letter = Char.ConvertFromUtf32(number);
+
+                this.lstList.Items.Add(number + "-" + letter);
+            }
+        }
+    }
+}
